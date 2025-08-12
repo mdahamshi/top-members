206a25d699dec060721c202a74727857e6e59ebf
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2025-08-12 22:54:42 +0300

    ui

--- client/src/pages/NotAuth.jsx
+++ client/src/pages/NotAuth.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 export default function NotAuth({ msg, link, title }) {
   return (
-    <div className="text-primary flex flex-col items-center justify-center p-8 font-sans text-cente dark:text-white">
-      <h1 className="text-4xl mb-4 font-bold">{title || 'Hey :)'}</h1>
+    <div className=" flex flex-col items-center justify-center p-8 font-sans text-cente dark:text-white">
+      <h1 className="text-4xl text-primary mb-4 font-bold">{title || 'Hey :)'}</h1>
 
-      <p className="text-black mb-4">{msg}</p>
+      <p className=" mb-4">{msg}</p>
       {link && (
         <Link to={`/${link.id}`} className="text-white link-btn min-w-22">
           {link.text}
